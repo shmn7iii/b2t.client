c2e56616b578620e1a297cf44af6f69ca72799fa
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-07-06 13:50:49 +0900

    update: add RPC methods and /health path

--- main.rb
+++ main.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/json'
+require 'bitcoin'
+require 'tapyrus'
+
+Bitcoin.chain_params = :signet
+Tapyrus.chain_params = :prod
+
+def bitcoinRPC
+  bitcoin_rpc_config = { schema: 'http', host: 'bitcoind', port: 38_332, user: 'hoge', password: 'hoge' }
+  Bitcoin::RPC::BitcoinCoreClient.new(bitcoin_rpc_config)
+end
+
+def tapyrusRPC
+  tapyrus_rpc_config = { schema: 'http', host: 'tapyrusd', port: 2377, user: 'hoge', password: 'hoge' }
+  Tapyrus::RPC::TapyrusCoreClient.new(tapyrus_rpc_config)
+end
+
 configure do
   set :bind, '0.0.0.0'
 end
@@ -9,3 +25,16 @@ get '/' do
   "Hi there! I'm client :)"
 end
 
+get '/health' do
+  data = {
+    bitcoin: {
+      chain: bitcoinRPC.getblockchaininfo['chain'],
+      blockcount: bitcoinRPC.getblockcount
+    },
+    tapyrus: {
+      chain: tapyrusRPC.getblockchaininfo['chain'],
+      blockcount: tapyrusRPC.getblockcount
+    }
+  }
+  json data
+end
